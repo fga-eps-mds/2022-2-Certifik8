ea52b1dcfa1555719678eaf681d38b0b7b8e4cb6
Leandro Silva <leanddrosouzasilva@gmail.com>
2022-12-07 14:17:36 -0300

    Skipe que converte o template em html para pdf
    Co-authored-by: Pedro Hnrique da Silva Melo <pedrodsm1819@gmail.com>

--- 2022-2-Squad08/main.py
+++ 2022-2-Squad08/main.py
@@ -1,1 +1,22 @@
-print("Conversão de html para pdf")
+from pathlib import Path
+import pdfkit
+import os
+#Funcao para converter html para pdf
+def convert_html_to_pdf(html_page, save_name):
+    pdfkit.from_file(html_page, save_name)
+
+#Funcao que pega o caminho para pasta download do sistema, podendo ser tanto Windows quanto Unix.
+def get_download_path():
+    if os.name == 'nt':
+        import winreg
+        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    else:
+        return os.path.join(os.path.expanduser('~'), 'Downloads/')
+
+download_folder = get_download_path()
+convert_html_to_pdf('2022-2-Squad08/certificado.html', download_folder + 'file.pdf')
+
